1d6f1707cfe0a03978cf2f2e1b5af990ce44c9ad
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-06-24 17:32:39 +0700

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const router = require('./router');
+const Socket = require('server/lib/socket');
+const socketAuth = require('./middlewares/socketAuth');
 
 module.exports = function (app) {
+    socketAuth(Socket.io);
+
     router.forEach(router => {
         app.use(router.routes());
     });
---
+++ middlewares/socketAuth.js
@@ -0,0 +1,43 @@
+const Cookies    = require('cookies');
+const jwt        = require('jsonwebtoken');
+const User       = require('users/models/user');
+const BlackToken = require('../models/blacktoken');
+
+module.exports = function (io) {
+    io.use(async (socket, next) => {
+        const tokens = getTokensFromSocket(socket);
+        const denied = await BlackToken.findOne({$or: [{token: String(tokens.access_token)}, {token: String(tokens.refresh_token)}]}).lean().exec();
+
+        if (denied) {
+            return next(new Error('Authentication error'));
+        }
+
+        try {
+            const userId = jwt.verify(tokens.access_token, process.env.SECRET).user_id;
+
+            const user = await User.findById(userId);
+
+            if (!user || !user.active) {
+                return next(new Error('Authentication error'));
+            }
+
+            socket.user = user;
+
+        } catch (e) {
+            return next(new Error('Authentication error'));
+        }
+
+        next()
+    });
+};
+
+function getTokensFromSocket (socket) {
+    const handshakeData = socket.request; // http(s) request
+    const cookies       = new Cookies(handshakeData, {}, {keys: process.env.KEYS});
+    const tokens        = {};
+
+    tokens.access_token  = cookies.get('x-access-token');
+    tokens.refresh_token = cookies.get('x-refresh-token');
+
+    return tokens;
+}
